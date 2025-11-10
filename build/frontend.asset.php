5abcbaadba55587f50c416fce835708df954827b
ThishanthSmakintel <143984999+ThishanthSmakintel@users.noreply.github.com>
2025-11-10 08:21:39 +0530

    onoging

--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data'), 'version' => '083198bdb4e95967b61e');
--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5cf57fcaace930a1120');
