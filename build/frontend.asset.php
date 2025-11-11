069533d7e66a1d807b810872b38d9e28f381eef1
ThishanthSmakintel <143984999+ThishanthSmakintel@users.noreply.github.com>
2025-11-11 07:16:37 +0530

    onignig

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data'), 'version' => 'b5765e38bb94e63e06cf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data'), 'version' => '12c7619d37e855b86d00');
